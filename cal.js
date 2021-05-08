ae2ea2c181b6682838194e1f225ba9c3cf222a95
Paul Sobolik <psobolik@zoho.com>
2021-05-08 11:15:27 -0400

    Upgrade Node packages

--- cal.js
+++ cal.js
@@ -1,3 +1,5 @@
+const { version } = require('grunt')
+
 ; (() => {
   'use strict'
 
@@ -5,27 +7,27 @@
 
   const calendar = require('./calendar')
 
-  var ArgumentParser = require('argparse').ArgumentParser
-  var argParser = new ArgumentParser({
-    version: '1.0.1',
-    addHelp: true,
+  const ArgumentParser = require('argparse').ArgumentParser
+  const argParser = new ArgumentParser({
+    add_help: true,
     description: 'Calendar printer'
   })
-  argParser.addArgument('-Y', { help: 'Display a calendar for the current year. (Ignore other arguments)', action: 'storeTrue' })
-  argParser.addArgument(['-x', '--extra'], { help: 'Display number of months before and after. Same as --before EXTRA --after EXTRA. (Default 0)' })
-  argParser.addArgument('-3', { help: 'Display prev/current/next months. Same as --extra 1.', action: 'storeTrue' })
-  argParser.addArgument(['-b', '--before'], { help: 'Display number of months before. (Default 0)' })
-  argParser.addArgument(['-a', '--after'], { help: 'Display number of months after. (Default 0)' })
-  argParser.addArgument(['-w', '--width'], { help: 'Display number of months per line. (Default 3)' })
-  argParser.addArgument(['-m', '--month'], { help: 'The month (1-12) to display. (Default current month)', requred: false })
-  argParser.addArgument(['-y', '--year'], { help: 'The year to display. (Default current year)', requred: false })
+  argParser.add_argument('-Y', { help: 'Display a calendar for the current year. (Ignore other arguments)', action: 'store_true' })
+  argParser.add_argument('-x', '--extra', { help: 'Display number of months before and after. Same as --before EXTRA --after EXTRA. (Default 0)' })
+  argParser.add_argument('-3', { help: 'Display prev/current/next months. Same as --extra 1.', action: 'store_true' })
+  argParser.add_argument('-b', '--before', { help: 'Display number of months before. (Default 0)' })
+  argParser.add_argument('-a', '--after', { help: 'Display number of months after. (Default 0)' })
+  argParser.add_argument('-w', '--width', { help: 'Display number of months per line. (Default 3)' })
+  argParser.add_argument('-m', '--month', { help: 'The month (1-12) to display. (Default current month)', required: false })
+  argParser.add_argument('-y', '--year', { help: 'The year to display. (Default current year)', required: false })
+  argParser.add_argument('-v', '--version', { action: 'version', version: '1.2.0' })
 
-  const args = argParser.parseArgs()
+  const args = argParser.parse_args()
 
   const now = new Date()
   let month = args.month ? --args.month : now.getMonth()
   let year = args.year || now.getFullYear()
-  let width = args.width || 3
+  const width = args.width || 3
 
   let monthCount = 1
   if (args.Y) {
